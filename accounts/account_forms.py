11be30a26c9156f924352f590b9c48fbd8331eb3
Bibek pandey <pandeybibek9849@gmail.com>
2023-12-07 17:08:10 +0545

    Project Setup with login and register

---
+++ accounts/account_forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class RegisterForm(forms.Form):
+    full_name=forms.CharField(max_length=100, required=True)
+    username=forms.CharField(max_length=25, required=True)
+    # contact_number=forms.CharField(max_length=10, required=True)
+    password=forms.CharField(max_length=15, min_length=6, required=True)
+    confirm_password=forms.CharField(max_length=15, min_length=6, required=True)
+    
+    def clean_username(self):
+        if User.objects.filter(username=self.cleaned_data.get("username")).exists():
+            raise forms.ValidationError("Username already exists")
+        return self.cleaned_data.get("username").strip()
+    
+    # def clean_contact_number(self):
+    #     if User.objects.filter(contact_number=self.cleaned_data.get("contact_number")).exists():
+    #         raise forms.ValidationError("This number already exists")
+    #     return self.cleaned_data.get("contact_number")
+        
+    def clean(self):
+        if self.cleaned_data.get("password")!=self.cleaned_data.get("confirm_password"):
+            raise forms.ValidationError("Password and Confirm password doesn't match")
+        return super().clean()
+    
+
+
+class LoginForm(forms.Form):
+    username=forms.CharField(max_length=25, required=True)
+    password=forms.CharField(max_length=15, min_length=6, required=True)
+    
+    def clean_username(self):
+        return self.cleaned_data.get("username").strip()
+        
+    def clean(self):
+        if User.objects.filter(username=self.cleaned_data.get("username"),password=self.cleaned_data.get("password")):
+            raise forms.ValidationError("Invalid username or password")
+        return super().clean()
---
+++ accounts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render,redirect
+from accounts.account_forms import RegisterForm, LoginForm
+# from django.contrib import messages
+from accounts.service.account_write import  create_user
+from django.contrib.auth import login, logout
+
+# Create your views here.
+def register_view(request):
+    register_form=RegisterForm
+    if request.method =='POST':
+        data = request.POST
+        form_data = register_form(data)
+        if form_data.is_valid():
+            user = create_user(form_data.cleaned_data)
+            login(request, user)
+            return redirect("login")
+        else:
+            print(register_form.errors)
+        print (form_data.errors)
+
+    return render(request,"account/register.html")
+
+def login_view(request):
+    register_form=LoginForm
+    if request.method =='POST':
+        data = request.POST
+        form_data = register_form(data)
+        if form_data.is_valid():
+            return redirect("home")
+        else:
+            print(register_form.errors)
+        print (form_data.errors)
+    return render(request, "account/login.html")
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'account/login.html')
