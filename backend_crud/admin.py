ac7414289146b80f9f05ae56b34016567af11e26
Bibek pandey <pandeybibek9849@gmail.com>
2023-12-13 20:15:32 +0545

    Implemented model for bus ticketing

--- backend_crud/admin.py
+++ backend_crud/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from backend_crud.models import BusStop, Route,Shedule, Bus, Driver
 # Register your models here.
+
+admin.site.register([BusStop, Route,Shedule, Bus, Driver])
--- backend_crud/models.py
+++ backend_crud/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class BusStop(models.Model):
+    stop_name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+
+class Route(models.Model):
+    from_location = models.CharField(max_length=50)
+    to_location = models.CharField(max_length=50)
+    route_name = models.CharField(max_length=50)
+    price = models.CharField(max_length=50)
+    stop = models.ForeignKey(BusStop, on_delete=models.CASCADE , null=True, blank=True)
+
+class Driver(models.Model):
+    driver_name = models.CharField(max_length=50)
+    driver_number = models.CharField(max_length=50)
+    lisence_number = models.CharField(max_length=50)
+    
+class Bus(models.Model):
+    bus_number = models.CharField(max_length=50)
+    bus_picture = models.ImageField()
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+
+class Shedule(models.Model):
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    departure_time = models.TimeField()
+    arrival_time = models.TimeField()
