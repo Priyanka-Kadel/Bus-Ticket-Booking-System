21e00aad7aeab61ebffb22cf93a93602f58f4744
Bibek pandey <pandeybibek9849@gmail.com>
2023-12-19 00:12:14 +0545

    Implemented Route  Search Filter

--- backend_crud/forms/search_route.py
+++ backend_crud/forms/search_route.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class SearchRouteForm(forms.Form):
-    to_location = forms.CharField()
-    from_location =forms.CharField()
+    to_location = forms.CharField(required=False)
+    from_location =forms.CharField(required=False)
     departure_time = forms.DateTimeField()
     
