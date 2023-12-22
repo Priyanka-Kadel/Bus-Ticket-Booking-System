b53f8103568906ae1d2b07300b8ff3ce3ecaf84b
Bibek pandey <pandeybibek9849@gmail.com>
2023-12-22 22:13:34 +0545

    bus route filtered by date

--- backend_crud/admin.py
+++ backend_crud/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from backend_crud.models import BusStop, Route,Schedule, Bus, Driver, BusSeatStatus
+from backend_crud.models import Route,Schedule, Bus, Driver, BusSeatStatus
 # Register your models here.
 
-admin.site.register([BusStop, Route,Schedule, Bus, Driver, BusSeatStatus])
+admin.site.register([Route,Schedule, Bus, Driver, BusSeatStatus])
--- backend_crud/views.py
+++ backend_crud/views.py
@@ -7,53 +7,38 @@ from django.db.models import Count, Case, When, IntegerField
 
 
 # Create your views here.
-
 def filter_route(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         search_form = search_route.SearchRouteForm(data=request.POST)
         if search_form.is_valid():
-            departure_time_str = search_form.data.get('departure_time')
-            departure_time = datetime.strptime(departure_time_str, '%Y-%m-%dT%H:%M')
-            # route_obj = Route.objects.prefetch_related(
-            #     Prefetch(
-            #         'schedule_set',
-            #         queryset=Schedule.objects.filter(
-            #             departure_time=date_object,
-            #             # busseatstatus__available=True
-            #         # ).prefetch_related(
-            #         #     Prefetch(
-            #         #         'busseatstatus_set',
-            #         #         queryset=BusSeatStatus.objects.filter(
-            #         #             available=True
-            #         #         ),
-            #         #         to_attr='bus_status'
-            #         #     )
-            #         ),
-            #         to_attr='schedules_with_status'
-            #     )
-            # )
-            schedules_with_buses_and_routes = Schedule.objects.select_related('bus', 'route')\
+            departure_date_str = search_form.data.get('departure_time')  
+            departure_date = datetime.strptime(departure_date_str, '%Y-%m-%d')
+
+            schedules_with_buses_and_routes = Schedule.objects.select_related('bus', 'route') \
                 .filter(route__to_location=search_form.data.get('to_location'),
                         route__from_location=search_form.data.get('from_location'),
-                          departure_time__exact  = departure_time).prefetch_related('busseatstatus_set').annotate(
-                                available_seats=Count(
-                                    Case(
-                                    When(busseatstatus__available=True, then=1),
-                                    output_field=IntegerField()
-                                    
-                                    )
-                                )
-                )  
-        route = Route.objects.values('from_location', 'to_location')
-        final_res = []
-        for data in route:
-            if data.get('from_location') not in final_res:
-                final_res.append(data.get('from_location'))
-            if  data.get('to_location') not in final_res:
-                final_res.append(data.get('to_location'))
+                        departure_time__date=departure_date) \
+                .prefetch_related('busseatstatus_set') \
+                .annotate(
+                    available_seats=Count(
+                        Case(
+                            When(busseatstatus__available=True, then=1),
+                            output_field=IntegerField()
+                        )
+                    )
+                )
 
-        return render(request, 'booking.html', context={'routes':schedules_with_buses_and_routes, 'locations':final_res})
-    return render(request, 'booking.html', context={'routes':[]})
+            route = Route.objects.values('from_location', 'to_location')
+            final_res = []
+            for data in route:
+                if data.get('from_location') not in final_res:
+                    final_res.append(data.get('from_location'))
+                if data.get('to_location') not in final_res:
+                    final_res.append(data.get('to_location'))
+
+            return render(request, 'booking.html', context={'routes': schedules_with_buses_and_routes,
+                                                            'locations': final_res})
+    return render(request, 'booking.html', context={'routes': []})
 
 def booking_view(request):
     route = Route.objects.values('from_location', 'to_location')
@@ -82,9 +67,10 @@ def seats_view(request, route_id):
     side_a = busseat.filter(seat_side = 'A')
     side_b = busseat.filter(seat_side = 'B')
 
+    print(busseat)
     return render(request, 'seats.html', context={'schedule':schedule,'side_a':side_a, 'side_b': side_b })
 
-def details_views(request):
+def details_views(request):   
     return render(request, 'details.html')
 
 
